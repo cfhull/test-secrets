0f66e49ee6b8f3fb881a9cdc1aa296c8a2c90540
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-04-14 10:28:26 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bil-map/precache-manifest.f643ecd867de25993be17794bee0a436.js"
+  "/bil-map/precache-manifest.13d9b9b32a3884ac0d052d80a3b7562f.js"
 );
 
 self.addEventListener('message', (event) => {
