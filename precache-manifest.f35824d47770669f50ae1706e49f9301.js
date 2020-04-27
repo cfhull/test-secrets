4766167ea793884e9877f1c90454f64bca5dc3fe
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-04-27 13:25:36 -0400

    Updates

---
+++ precache-manifest.f35824d47770669f50ae1706e49f9301.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c745a48e3631203ba45b521c3611ea0",
+    "url": "/bil-map/index.html"
+  },
+  {
+    "revision": "d348578179e595cf0eb0",
+    "url": "/bil-map/static/css/main.f7f48eb5.chunk.css"
+  },
+  {
+    "revision": "73effb18598d4a20f61c",
+    "url": "/bil-map/static/js/2.ecf17f2f.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/bil-map/static/js/2.ecf17f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d348578179e595cf0eb0",
+    "url": "/bil-map/static/js/main.41d083a7.chunk.js"
+  },
+  {
+    "revision": "df6d84309ee1ca332b0a",
+    "url": "/bil-map/static/js/runtime-main.8860a1e1.js"
+  },
+  {
+    "revision": "23bb7377d3992e0b67ad0f719b50f899",
+    "url": "/bil-map/static/media/mapboxgl-ctrl-attrib.23bb7377.svg"
+  },
+  {
+    "revision": "08806268d9bac29ace8d925d7e235848",
+    "url": "/bil-map/static/media/mapboxgl-ctrl-logo.08806268.svg"
+  },
+  {
+    "revision": "0b972bc0aeeb5bb2abec12bcaf3e5fd3",
+    "url": "/bil-map/static/media/mapboxgl-ctrl-zoom-in.0b972bc0.svg"
+  },
+  {
+    "revision": "6a52a7aab97b1b05ceb76587ab13f97f",
+    "url": "/bil-map/static/media/mapboxgl-ctrl-zoom-out.6a52a7aa.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bil-map/precache-manifest.86597dd59a698702db03180e74e2fe58.js"
+  "/bil-map/precache-manifest.f35824d47770669f50ae1706e49f9301.js"
 );
 
 self.addEventListener('message', (event) => {
